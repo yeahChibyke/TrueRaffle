57ac800df18edffc03eba18912e2f00e1d14fd91
yeahChibyke <chibykepride@gmail.com>
2024-03-06 18:52:45 +0100

    first commit

--- README.md
+++ README.md
@@ -1,66 +1,15 @@
-## Foundry
+# TrueRaffle 
 
-**Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
+## About
 
-Foundry consists of:
+This code is to create a proveably random smart contract lottery
 
--   **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
--   **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
--   **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
--   **Chisel**: Fast, utilitarian, and verbose solidity REPL.
+## What do I want it to do?
 
-## Documentation
-
-https://book.getfoundry.sh/
-
-## Usage
-
-### Build
-
-```shell
-$ forge build
-```
-
-### Test
-
-```shell
-$ forge test
-```
-
-### Format
-
-```shell
-$ forge fmt
-```
-
-### Gas Snapshots
-
-```shell
-$ forge snapshot
-```
-
-### Anvil
-
-```shell
-$ anvil
-```
-
-### Deploy
-
-```shell
-$ forge script script/Counter.s.sol:CounterScript --rpc-url <your_rpc_url> --private-key <your_private_key>
-```
-
-### Cast
-
-```shell
-$ cast <subcommand>
-```
-
-### Help
-
-```shell
-$ forge --help
-$ anvil --help
-$ cast --help
-```
+- Users enter the raffle by paying for a ticket
+  - Ticket fees goes to the winner after the draw
+- After a set period of time the lottery will automatically draw a winner
+  - And this will be done programmatically
+- Utilize Chainlink VRF and Chainlink Automation
+  - Chainlink VRF ----> Randomness
+  - Chainlink Automation ----> Time-based trigger
---
+++ src/TrueRaffle.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+/** Imports */
+
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+
+/** Errors */
+
+error TrueRaffle__NotEnoughETHSent();
+
+pragma solidity ^0.8.20;
+
+/**
+ * @title TrueRaffle
+ * @author yeahChibyke
+ * @notice Raffle system contract
+ * @dev implements Chainlink VRFv2
+ */
+contract TrueRaffle is VRFConsumerBaseV2 {
+    /** State Variables */
+
+    uint16 private constant TRUE_REQUESTCONFIRMATIONS = 3;
+    uint32 private constant TRUE_NUMWORDS = 1;
+
+    uint256 private immutable i_TrueEntranceFee;
+    // @dev: duration of the lottery in seconds
+    uint256 private immutable i_TrueTimeInterval;
+    // @dev: Chainlink VRF
+    VRFCoordinatorV2Interface private immutable i_TrueVRFCoordinator;
+    bytes32 private immutable i_TrueGasLane;
+    uint64 private immutable i_TrueSubscriptionID;
+    uint32 private immutable i_TrueCallbackGasLimit;
+
+    address payable[] private s_TruePlayers;
+    uint256 private s_TrueLastTimeStamp;
+
+    /** Events */
+
+    event EnteredTrueRaffle(address indexed TruePlayer);
+
+    constructor(
+        uint256 entranceFee,
+        uint256 timeInterval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionID,
+        uint32 callbackGasLimit
+    ) {
+        i_TrueEntranceFee = entranceFee;
+        i_TrueTimeInterval = timeInterval;
+        s_TrueLastTimeStamp = block.timestamp;
+        i_TrueVRFCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_TrueGasLane = gasLane;
+        i_TrueSubscriptionID = subscriptionID;
+        i_TrueCallbackGasLimit = callbackGasLimit;
+    }
+
+    function enterTrueRaffle() external payable {
+        if (msg.value < i_TrueEntranceFee) {
+            revert TrueRaffle__NotEnoughETHSent();
+        }
+        s_TruePlayers.push(payable(msg.sender));
+        emit EnteredTrueRaffle(msg.sender);
+    }
+
+    function pickTrueWinner() external {
+        // check to ensure adequate time has passed
+        if ((block.timestamp - s_TrueLastTimeStamp) < i_TrueTimeInterval) {
+            revert();
+        }
+        uint256 requestId = i_TrueVRFCoordinator.requestRandomWords(
+            i_TrueGasLane, // gasLane
+            i_TrueSubscriptionID,
+            TRUE_REQUESTCONFIRMATIONS,
+            i_TrueCallbackGasLimit,
+            TRUE_NUMWORDS
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
+
+    /** Getter Functions */
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_TrueEntranceFee;
+    }
+}
